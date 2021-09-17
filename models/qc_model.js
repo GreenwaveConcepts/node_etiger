d87417eb4dc35b37f0ed5f15fa8b2fb770b06162
Bradley Armstrong <54115682+barmstrong-truenergy@users.noreply.github.com>
2021-09-17 15:59:56 -0500

    cb

--- models/qc_model.js
+++ models/qc_model.js
@@ -2,6 +2,8 @@ const vtiger = require('vtiger');
 const user_model = require('../models/users_model');
 module.exports = {
     pullLeadInfo: (record, user) => {
+        var key;
+        var userName;
         user_model.userProfile(user, (data) => {
             key = data.accesskey;
             userName = data.user_name
